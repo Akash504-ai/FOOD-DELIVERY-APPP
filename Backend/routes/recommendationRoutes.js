4e2fb9a3be31e476d4571cab423f27443c25cdc4
Akash Santra <santraakash999@gmail.com>
2026-03-07 23:25:06 +0530

    rec route

--- Backend/routes/recommendationRoutes.js
+++ Backend/routes/recommendationRoutes.js
@@ -2,7 +2,5 @@ import express from "express";
 import { recommendItems } from "../controllers/recommendationController.js";
 
 const router = express.Router();
-
 router.get("/recommend/:itemId", recommendItems);
-
 export default router;
