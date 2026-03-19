be09c9eee113b8f8b592fe84ae2c1c121d3a5b9d
Akash Santra <santraakash999@gmail.com>
2026-03-19 15:37:25 +0530

    recomendation updt

--- Frontend/src/services/recommendationService.js
+++ Frontend/src/services/recommendationService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getRecommendations = async (itemId) => {
   const res = await axios.get(
-    `http://localhost:5000/api/recommend/${itemId}`
+    `https://food-delivery-appp-1.onrender.com/api/recommend/${itemId}`
   );
 
   return res.data.recommendations;
