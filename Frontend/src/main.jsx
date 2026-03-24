2bc3db3d1e4d840c2a16c7248d60eed26c8c3ca3
Akash Santra <santraakash999@gmail.com>
2026-03-24 12:13:20 +0530

    fix 1

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 import axios from "axios"   
 
-axios.defaults.withCredentials = true  
+// axios.defaults.withCredentials = true  
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
